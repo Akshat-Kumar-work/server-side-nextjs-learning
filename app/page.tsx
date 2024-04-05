063a425775a69aebf80115716af38fb1a50d2251
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-05 11:58:19 +0530

    server side db call and diff  interaction

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
 
 //data fetching is happening on next js server , 
 //this file is server side not client side 
@@ -6,19 +8,49 @@ import axios from "axios";
 //server component can directly talk to another backend
 
 
+
+//interacting with database through api call of next js server
 async function getUserDetails(){
   const response = await axios.get("http://localhost:3000/api/user")
   return response.data;
 }
 
+//interacting with database directly not from next js server api call
+async function fetchData(){
+  const user = await prisma.user.findFirst();
+  return{
+    name:user?.username,
+    password:user?.password
+  }
+}
+
 //creating an async components only in server side
 export default async function Home() {
   const userDetails = await getUserDetails();
+  const data = await fetchData();
   return (
- <div className=" flex flex-col items-center ">
+ <div className=" flex flex-col items-center p-8 gap-6 ">
        <div> home page</div> 
-        {userDetails.email}
-        {userDetails.name}
- </div>
-  );
+
+      {/* data of api call of next js backend server throught http endpoints */}
+       <div className=" flex flex-col">
+       Data from next js server api call
+       <div className=" flex justify-between">
+      <div> {userDetails.email}</div>
+        <div>{userDetails.name}</div>
+       </div>
+       </div>
+
+
+      {/* getting data directly from server to db */}
+       <div className=" flex flex-col">
+        data from direct interaction with db without next js server api call
+          <div className=" flex justify-between">
+          <div>{data.name}</div>
+          <div>{data.password}</div>
+          </div>
+     </div>
+     </div>
+     )
+  
 }
