0bb5afc624153ca727be7d39cd5ff92cbd9a9655
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-10 00:19:32 +0530

    selective middlewares

---
+++ app/middlewareRoute/page.tsx
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+  return (
+    <div>Admin route se redirect hokar aye ho due to middleware 
+        i had used between the request
+    </div>
+  )
+}
+
+export default page
--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 
 let reqCount = 0;
-//keeping  count of each and every request 
+
 export  function  middleware(req:NextRequest){
 
     //getting request url
     console.log(req.nextUrl.pathname)
     //agar req ka url /admin se start hota hai toh usko signin route p redirect krdo
   if (req.nextUrl.pathname.startsWith('/admin')) {
-    return NextResponse.redirect(new URL('/signin', req.url))
+    console.log("inside admin")
+    return NextResponse.redirect(new URL('/middlewareRoute', req.url))
   }
  
-  if (req.nextUrl.pathname.startsWith('/dashboard')) {
+  if (req.nextUrl.pathname.startsWith('/akshat')) {
     return NextResponse.next()
   }
 
+//keeping  count of each and every request 
 
     reqCount++;
     const res = NextResponse.next();
-    console.log("reqcount is ",reqCount)
+   //console.log("reqcount is ",reqCount)
     return res;
 }
 
 //selective middleware
-//this means if the route will hit signup/* etc then only this middleware will run otherwise no
-export const config = {
-    matcher: '/signup/:path*'
-}
+//run middleware only selective path
+//this means if the route will hit signup/* etc then only this whole middleware will run otherwise it will not affect any requests
+// export const config = {
+//     matcher: '/signup/:path*'
+// }
 
  
 
